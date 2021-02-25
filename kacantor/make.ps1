42409fbca101574e023ed6f63965cc3b215a8ca9
eulerphi <jacowhig@gmail.com>
2021-02-25 12:06:30 -0800

    Update make.ps1
    Add "deploy" script to make.

--- kacantor/make.ps1
+++ kacantor/make.ps1
@@ -1,6 +1,11 @@
 if ($args[0] -eq "live") {
     elm-live src/Main.elm --open '--' --output build/app.js --debug
 }
+elseif ($args[0] -eq "deploy") {
+    Copy-Item .\index.html ..\..\eulerphi.github.io\kacantor\
+    Copy-Item .\style.css ..\..\eulerphi.github.io\kacantor\
+    Copy-Item .\build\app.js ..\..\eulerphi.github.io\kacantor\build
+}
 else {
     elm make src/Main.elm --output build/app.js
 }
